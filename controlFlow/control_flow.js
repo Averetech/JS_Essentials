b017cde5aee7336d55803ce5f3a2ff47e4e62aca
Averetech <mbanezenande@gmail.com>
2024-10-18 06:14:48 -0400

    added js control flow and if statements

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,46 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = `The ${userRole} is granted full access`;
+} else {
+    console.log(`This isn't true for ${userRole}`);
+}
+
+console.log(accessLevel);
+
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+    userMessage = `Welcome, ${userRole}! You are successfully logged in.`;
+    } else if (userRole === "supervisor") {
+        userMessage = `Welcome, supervisor! You are successfully logged in.`;
+    }
+} else {
+    userMessage = "Your login has been unsuccessful.";
+}
+
+console.log(userMessage);
+
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "subscriber":
+        userCategory = `Thank you for subscribing to our channel 😊.`;
+    break;
+    case "admin":
+        userCategory = `Welcome, admin! You have full access on our channel`;
+    break;
+    case "supervisor":
+        userCategory = `Welcome, supervisor! Your access is limited on our channel.`;
+    break;
+    default:
+        userCategory = `Please perform some action to become a user. Thank you!`;
+}
+
+console.log(userCategory);
