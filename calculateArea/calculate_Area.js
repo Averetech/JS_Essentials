4dd79ef3d80856660791d2157adc5b36fcffd263
Averetech <mbanezenande@gmail.com>
2024-10-22 05:26:03 -0400

    calculated the total price of items to make tea

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,20 @@ function calculateArea() {
     let area = length * width;
     document.getElementById("result").innerHTML = `The calculated is equal to ${area}.`;
 }
+
+
+//Practice task
+let milk;
+let coffee;
+let sugar;
+
+function groceryTracker() {
+    milk = parseFloat(document.getElementById("milk").value);
+    coffee = parseFloat(document.getElementById("coffee").value);
+    sugar = parseFloat(document.getElementById("sugar").value);
+
+    let total = milk + coffee + sugar;
+
+    document.getElementById("total-amount").innerHTML = `The amount you need to get all the above items is ${total}`
+}
+
